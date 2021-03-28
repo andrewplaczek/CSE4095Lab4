001f516c25d58a217df0b3708c1e9551109f5e30
Andrew Placzek <andrew.placzek@uconn.edu>
2021-03-28 17:16:15 -0400

    Updated Application

--- app/src/main/java/me/pgb/a2021_03_17_radio/MainActivity.java
+++ app/src/main/java/me/pgb/a2021_03_17_radio/MainActivity.java
@@ -1,5 +1,6 @@
 package me.pgb.a2021_03_17_radio;
 
+import android.content.Context;
 import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioManager;
@@ -18,22 +19,31 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Parcelable;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MAIN__";
     private MediaPlayer mediaPlayer;
-    private static final String url = "http://stream.whus.org:8000/whusfm"; //";//http://vprbbc.streamguys.net:80/vprbbc24.mp3";
+    private String url = "http://stream.whus.org:8000/whusfm"; //";//http://vprbbc.streamguys.net:80/vprbbc24.mp3";
     private Button internetRadioButton;
+    private Spinner stationSpinner;
     private boolean radioOn;
     private boolean radioWasOnBefore;
+    private SeekBar volumeSeekBar;
+    private AudioManager mgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +86,94 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        //Set up station selection dropdown menu
+        stationSpinner = findViewById(R.id.station_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.radio_stations, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stationSpinner.setAdapter(adapter);
+
+        stationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0){
+                    url = "http://stream.whus.org:8000/whusfm";
+                }
+                else if (i == 1){
+                    url = "http://vprbbc.streamguys.net:80/vprbbc24.mp3";
+                }
+                else if (i == 2){
+                    url = "http://playerservices.streamtheworld.com/api/livestream-redirect/WTICFM.mp3";
+                }
+                else if (i == 3){
+                    url = "http://stream.revma.ihrhls.com/zc433/hls.m3u8";
+                }
+                else if (i == 4){
+                    url = "http://stream.revma.ihrhls.com/zc445/hls.m3u8";
+                }
+                if (radioOn) { // ON so Turn OFF
+                    if (mediaPlayer.isPlaying()) {
+                        Log.i(TAG, "Radio is playing- turning off " );
+                        radioWasOnBefore = true;
+                    }
+                    mediaPlayer.pause();
+                    if (!mediaPlayer.isPlaying()) {
+                        if (radioWasOnBefore) {
+                            mediaPlayer.release();
+                            mediaPlayer = new MediaPlayer();
+                        }
+                        radioSetup(mediaPlayer);
+                        mediaPlayer.prepareAsync();
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //PASS - Don't do anything if nothing is selected
+            }
+        });
+
+        //Set Up Volume Slider
+        volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        try{
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            mgr = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+            volumeSeekBar.setMax(mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            volumeSeekBar.setProgress(mgr.getStreamVolume(AudioManager.STREAM_MUSIC));
+            volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    mgr.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //Sync volume bar with volume buttons
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP){
+            volumeSeekBar.setProgress((volumeSeekBar.getProgress()+1>volumeSeekBar.getMax())?volumeSeekBar.getMax():volumeSeekBar.getProgress()+1);
+        }
+        else if (keyCode==KeyEvent.KEYCODE_VOLUME_DOWN){
+            volumeSeekBar.setProgress((volumeSeekBar.getProgress()-1<0)?0:volumeSeekBar.getProgress()-1);
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 
     public void radioSetup(MediaPlayer mediaPlayer) {
